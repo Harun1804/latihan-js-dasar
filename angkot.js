bf77b103409f882559dc8e2bede48e4112e680ee
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-08-03 10:37:00 +0700

    Tambah Opsi Kondisi

--- angkot.js
+++ angkot.js
@@ -2,9 +2,9 @@ var jmlAngkot = 10;
 var angkotBeroperasi = 6;
 
 for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
-  if(noAngkot <= angkotBeroperasi){
+  if(noAngkot <= 6 && noAngkot !== 5){
     console.log("Angkot No. "+noAngkot+" Sedang Beroperasi");
-  }else if(noAngkot === 8){
+  }else if(noAngkot === 8 || noAngkot === 10 || noAngkot === 5){
     console.log("Angkot No. "+noAngkot+" Sedang Lembur");
   }else{
     console.log("Angkot No. "+noAngkot+" Sedang Tidak Beroperasi");
