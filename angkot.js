ce131fc76f3c74e34c4bc3a3915136883030e3d3
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-08-03 09:58:49 +0700

    Elseif

--- angkot.js
+++ angkot.js
@@ -4,6 +4,8 @@ var angkotBeroperasi = 6;
 for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
   if(noAngkot <= angkotBeroperasi){
     console.log("Angkot No. "+noAngkot+" Sedang Beroperasi");
+  }else if(noAngkot === 8){
+    console.log("Angkot No. "+noAngkot+" Sedang Lembur");
   }else{
     console.log("Angkot No. "+noAngkot+" Sedang Tidak Beroperasi");
   }
