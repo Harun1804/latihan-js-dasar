74bbecbecadd9d2758beb895cc56a71ba34ea9b7
Harun1804 <Harun.arrasyid1804@gmail.com>
2021-08-03 09:29:00 +0700

    Update Angkot

---
+++ angkot/angkot.js
@@ -0,0 +1,12 @@
+var jmlAngkot = 10;
+var noAngkot = 1;
+var angkotBeroperasi = 6
+
+while (noAngkot <= angkotBeroperasi) {
+  console.log("Angkot No. "+noAngkot+" Beroperasi Dengan Baik");
+  noAngkot++;
+}
+
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+  console.log("Angkot No. "+noAngkot+" Sedang Tidak Beroperasi");
+}
--- angkot/angkot1.js
+++
@@ -1,6 +0,0 @@
-var jumlahAngkot = 10;
-var noAngkot = 1;
-while (noAngkot <= jumlahAngkot) {
-  console.log("Angkot No. "+noAngkot+" Beroperasi Dengan Baik");
-  noAngkot++;
-}
